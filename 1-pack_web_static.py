0f48d882b3b33980e3f32c7a21e746260e3ae4ea
makterince <fomaku3@gmail.com>
2023-05-04 15:01:56 -0700

    fabric to web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This script generates a .tgz archive from web_satic folder """
+from fabric.api import local, env
+from datetime import datetime
+
+env.hosts = ['54.165.197.124']
+env.user = 'makterince'
+env.key_filename = '/ssh/id_rsa.pub'
+
+def do_pack():
+    """Create a tgz archive from the contents of web_static folder"""
+    try:
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(current_time)
+        
+        local("mkdir -p versions")
+        local("tar -cvzf {} web_static".format(archive_path))
+        
+        return archive_path
+
+    except:
+        return None
