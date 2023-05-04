f0f07a74daf16f7f9db7fb7e69004cd24e92ccb3
makterince <fomaku3@gmail.com>
2023-05-04 16:25:22 -0700

    tired already

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers."""
-
-from os.path import isfile
-from fabric.api import env, put, run
+""" This script distributes an archive to webservers"""
+from fabric.api import local, put, run, env, runs_once
 from datetime import datetime
+from os.path import isfile
 
-env.hosts = ['54.165.197.124', '54.234.35.46']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
+env.hosts = ["54.165.197.124", "54.234.35.46"]
+env.user = "ubuntu"
+
+@runs_once
 def do_pack():
     """ Generates a .tgz archive """
     
     time_now = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "web_static_{}.tgz".format(time_now)
     archive_path = "versions/{}".format(filename)
-
+    
     print("Packing web_static to {}".format(archive_path))
-
+    
     local("mkdir -p versions")
     result = local("tar -cvzf {} web_static".format(archive_path))
-
     if result.failed:
         return None
-    
     print("Successfully packed web_static to {}".format(archive_path))
     
     return archive_path
@@ -33,7 +31,6 @@ def do_deploy(archive_path):
     
     if not isfile(archive_path):
         return False
-    
     print("Deploying new version")
     
     archive_name = archive_path.split("/")[-1]
@@ -51,7 +48,16 @@ def do_deploy(archive_path):
     run("rm -rf /tmp/{} {}/web_static".format(archive_name, dir_path))
     run("rm -rf /data/web_static/current")
     run("ln -s {} /data/web_static/current".format(dir_path))
-
+    
     print("New version deployed!")
-
+    
     return True
+
+def deploy():
+    """ Performs a full deployment from generating archive to deploying """
+    
+    filename = do_pack()
+    if not filename:
+        return False
+    
+    return do_deploy(filename)
