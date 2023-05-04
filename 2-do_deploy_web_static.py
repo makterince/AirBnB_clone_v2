b5738f85d434e46b40198f1f4f5d62c1807cd870
makterince <fomaku3@gmail.com>
2023-05-04 15:45:35 -0700

    git fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Fabric script that distributes an archive to your web servers."""
 
-import os
+from os.path import isfile
 from fabric.api import env, put, run
 from datetime import datetime
 
