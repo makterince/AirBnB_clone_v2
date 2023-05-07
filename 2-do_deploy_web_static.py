b7ed9ae01f165678d43b8546dfba127859c52008
makterince <fomaku3@gmail.com>
2023-05-07 06:53:18 -0700

    fix again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,9 +9,10 @@ env.hosts = ['54.165.197.124', '54.234.35.46']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
+
 def do_pack():
     """ Generates a .tgz archive """
-    
+
     time_now = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "web_static_{}.tgz".format(time_now)
     archive_path = "versions/{}".format(filename)
@@ -23,30 +24,31 @@ def do_pack():
 
     if result.failed:
         return None
-    
+
     print("Successfully packed web_static to {}".format(archive_path))
-    
+
     return archive_path
+
 
 def do_deploy(archive_path):
     """ Distributes an archive to webserver """
-    
+
     if not isfile(archive_path):
         return False
-    
+
     print("Deploying new version")
-    
+
     archive_name = archive_path.split("/")[-1]
     folder_name = archive_name[: -4]
     dir_path = "/data/web_static/releases/{}".format(folder_name)
-    
+
     put(archive_path, "/tmp/")
     run("mkdir -p {}".format(dir_path))
     result = run("tar -xzf /tmp/{} -C {}".format(archive_name, dir_path))
-    
+
     if result.failed:
         return False
-    
+
     run("cp -r {}/web_static/* {}".format(dir_path, dir_path))
     run("rm -rf /tmp/{} {}/web_static".format(archive_name, dir_path))
     run("rm -rf /data/web_static/current")
