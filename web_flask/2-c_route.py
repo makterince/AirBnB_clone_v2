7b3338458eda203b83260991964cabb338341a67
makterince <fomaku3@gmail.com>
2023-05-21 03:08:25 -0700

    format space

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-""" starts a flask web application """
+""" This script starts a flask web application """
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ route handler """
+    """ home route configuration """
+
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """ route handler """
+    """ hbnb route configuration """
+
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """replace underscore with whitespace """
-    format_text = text.replace('_', ' ')
-    return f'C {format_text}'
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """ c route configuration """
+
+    return "C {}".format(text.replace("_", " "))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
