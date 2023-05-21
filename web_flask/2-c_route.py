b8abf6d1517dd17ac36aa568bc9d47bdd3357fa8
makterince <fomaku3@gmail.com>
2023-05-21 03:02:37 -0700

    format space

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" starts a flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ route handler """
+    return "Hello HBNB!"
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """ route handler """
+    return "HBNB"
+
+
+@app.route('/', strict_slashes=False)
+def c(text):
+    """replace underscore with whitespace """
+    format_text = text.replace('_', ' ')
+    return f'C {format_text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
