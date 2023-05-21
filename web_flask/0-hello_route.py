1d5332ea10a4a659422780cc98d4ef37fff6daf5
makterince <fomaku3@gmail.com>
2023-05-21 02:49:07 -0700

    2 routers

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,11 +8,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ initializing the  routing handler for root url"""
-    
-
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    """ start flask application"""
     app.run(host='0.0.0.0', port=5000)
